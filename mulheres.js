c8be5cc4514f0568490392ba9176b8f82d310f86
stephmarchuk <stephmarchuk15@gmail.com>
2023-05-07 11:33:37 -0300

    correcao app listen porta

--- mulheres.js
+++ mulheres.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //iniciando o express
+const router = express.Router() //configurando a primeira parte da rota
 
-const app = express()
-const porta = 3333
+const app = express() //iniciando o app
+const porta = 3333 // iniciando a porta
+
+//abaixo crio a lista inicial de mulheres
 const mulheres = [
     {
         nome: 'Stéphanie Marchuk',
@@ -15,18 +17,21 @@ const mulheres = [
         minibio: 'pesquisadora e ilustradora'
     },
     {
-        nome: 'dilma cachorrinha',
+        nome: 'Nina da Hora',
         imagem: 'https://media.licdn.com/dms/image=pOJSiGg-hxz0KdOIZxQhPVMOFh-7jsedml_HQPtQtHM',
-        minibio: 'pesquisadora e ilustradora'
+        minibio: 'Hacker Antiracista'
     }
 ]
 
+//GET
 function mostraMulheres(request,response) {
     response.json(mulheres)
 }
 
+//PORTA
 function mostraPorta() {
     console.log('Servidor criado e rodando na porta', porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
+app.use(router.get('/mulheres', mostraMulheres)) //configurei rota GET/mulheres
+app.listen(porta, mostraPorta) //servidor ouvindo porta
